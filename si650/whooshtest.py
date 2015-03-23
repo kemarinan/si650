4852f7066a4a927c11daf062444ecc4772d05b0c
Jessica <jebene@umich.edu>
2015-03-23 16:02:45 -0400

    small cleanup

--- si650/whooshtest.py
+++ si650/whooshtest.py
@@ -7,16 +7,21 @@ schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(stor
 
 if not os.path.exists("index"):
     os.mkdir("index")
+
 ix = create_in("index", schema)
 
 writer = ix.writer()
 # This could be replaced with a loop over files to create docs for each concept
-writer.add_document(title=u"First document", path=u"/a",
-                content=u"This is the first document we've added!")
-writer.add_document(title=u"Second document", path=u"/b",
-                     content=u"The second one is even more interesting! It says firstly too")
-writer.add_document(title=u"Third document", path=u"/c",
-                content=u"This is the third document we've added!")
+writer.add_document(title = "First document",
+                    path = "/a",
+                    content = "This is the first document we've added!")
+writer.add_document(title = "Second document",
+                    path = "/b",
+                    content = ("The second one is even more interesting!"
+                               "It says firstly too"))
+writer.add_document(title = "Third document",
+                    path = "/c",
+                    content = "This is the third document we've added!")
 writer.commit()
 
 
@@ -27,19 +32,9 @@ with ix.searcher() as searcher:
     results = searcher.search(q, limit=None, terms=True)
     print(results[:])
 
-    print(len(results))
-    print(results.scored_length())
+    for hit in results:
+        print(hit.highlights("content"))
 
-	# Was this results object created with terms=True?
-    if results.has_matched_terms():
-    # What terms matched in the results?
-        print(results.matched_terms())
-           # What terms matched in each hit?
-        for hit in results:
-            print(hit.matched_terms())
-            print(hit.highlights("content"))
-#{"title": u"First document", "path": u"/a"}
+#TODO: Add in filters for ontology type
 
-#Add in filters for ontology type
-#Change tabs to spaces
 
--- test/indexer.py
+++
@@ -1,1 +0,0 @@
-from whoosh.fields import
