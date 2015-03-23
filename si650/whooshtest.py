a5b847d3829c64f2fac74ed889f5ab75cd6cd783
Jessica <jebene@umich.edu>
2015-03-23 16:14:13 -0400

    fixed unicode bug

--- si650/whooshtest.py
+++ si650/whooshtest.py
@@ -12,16 +12,16 @@ ix = create_in("index", schema)
 
 writer = ix.writer()
 # This could be replaced with a loop over files to create docs for each concept
-writer.add_document(title = "First document",
-                    path = "/a",
-                    content = "This is the first document we've added!")
-writer.add_document(title = "Second document",
-                    path = "/b",
-                    content = ("The second one is even more interesting!"
+writer.add_document(title = u"First document",
+                    path = u"/a",
+                    content = u"This is the first document we've added!")
+writer.add_document(title = u"Second document",
+                    path = u"/b",
+                    content = (u"The second one is even more interesting!"
                                "It says firstly too"))
-writer.add_document(title = "Third document",
-                    path = "/c",
-                    content = "This is the third document we've added!")
+writer.add_document(title = u"Third document",
+                    path = u"/c",
+                    content = u"This is the third document we've added!")
 writer.commit()
 
 
