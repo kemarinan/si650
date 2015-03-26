5e80699c362cb80219528906b31d77d26d688e93
Jessica <jebene@umich.edu>
2015-03-26 10:50:09 -0400

    edited scripts to match tsv format

--- si650/index_documents.py
+++ si650/index_documents.py
@@ -2,6 +2,7 @@
 import argparse
 import glob
 import os
+import re
 import sys
 
 from whoosh.fields import *
@@ -11,14 +12,17 @@ def _add_document(input_file, writer):
     file_reader = open(input_file, "r")
     for i, line in enumerate(file_reader.readlines()):
         doc_title = "_".join([os.path.basename(input_file), str(i + 1)])
+        ontology, unique_id, doc_content = line.split("\t")
         writer.add_document(title=unicode(doc_title,"UTF-8"),
-                            content=unicode(line, "UTF-8"))
+#                             tags=unicode(tag_name, "UTF-8"),
+                            content=unicode(doc_content, "UTF-8"))
     writer.commit()
 
 def _create_writer(index_dir):
     schema = Schema(title=TEXT(stored=True),
                     path=ID(stored=True),
-                    content=TEXT(stored=True))
+                    content=TEXT(stored=True),
+                    tags=TEXT(stored=True))
 
     ix = create_in(index_dir, schema)
     writer = ix.writer()
--- si650/query_data.py
+++ si650/query_data.py
@@ -5,6 +5,7 @@ import sys
 
 from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
+from whoosh.query import Term
 
 def _open_index():
     script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -13,8 +14,12 @@ def _open_index():
 
     return ix
 
-def _get_results(searcher, query):
-    results = searcher.search(query, limit=None, terms=True)
+def _get_results(searcher, query_term, filter_term):
+    allow_query = Term("tags", filter_term)
+    if filter_term:
+        results = searcher.search(query_term, filter=allow_query, limit=None, terms=True)
+    else:
+        results = searcher.search(query_term, limit=None, terms=True)
     print(results[:])
 
     for hit in results:
@@ -23,18 +28,22 @@ def _get_results(searcher, query):
 def _query_data(args):
     ix =  _open_index()
 
-    query_term = args.query
+    user_query= args.query
+    filter_term = args.filter_results
     query_parser = QueryParser("content", schema=ix.schema)
-    query = query_parser.parse(unicode(query_term, "UTF-8"))
+    query_term = query_parser.parse(unicode(user_query, "UTF-8"))
     
     with ix.searcher() as searcher:
-        _get_results(searcher, query)
+        _get_results(searcher, query_term, filter_term)
 
 def _add_arg_parse():
     command_line_args = sys.argv[1:]
     parser = argparse.ArgumentParser()
-    parser.add_argument("query", help=("Query term to be searched against "
-                                       "the indexed documents"))
+    parser.add_argument("query",
+                        help=("Query term to be searched against the indexed "
+                              "documents"))
+    parser.add_argument("--filter_results",
+                        help=("Ontology name to filter the results by"))
 
     return parser.parse_args(command_line_args)
 
