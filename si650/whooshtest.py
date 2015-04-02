18698b7e6991f6a435f8b2a846dc062507995b73
changlaw <changlaw@umich.edu>
2015-04-02 11:12:47 -0500

    small changes

--- si650/whooshtest.py
+++ si650/whooshtest.py
@@ -10,18 +10,19 @@ if not os.path.exists("index"):
 
 ix = create_in("index", schema)
 
+#u added to strings
 writer = ix.writer()
 # This could be replaced with a loop over files to create docs for each concept
-writer.add_document(title = "First document",
-                    path = "/a",
-                    content = "This is the first document we've added!")
-writer.add_document(title = "Second document",
-                    path = "/b",
-                    content = ("The second one is even more interesting!"
-                               "It says firstly too"))
-writer.add_document(title = "Third document",
-                    path = "/c",
-                    content = "This is the third document we've added!")
+writer.add_document(title = u"First document",
+                    path = u"/a",
+                    content = u"This is the first document we've added!")
+writer.add_document(title = u"Second document",
+                    path = u"/b",
+                    content = (u"The second one is even more interesting!"
+                               u"It says firstly too"))
+writer.add_document(title = u"Third document",
+                    path = u"/c",
+                    content = u"This is the third document we've added!")
 writer.commit()
 
 
